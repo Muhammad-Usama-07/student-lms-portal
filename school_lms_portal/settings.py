a0afeb013852a9bbe5e8b3fc3965fc2f101278a8
Muhammad Usama <51862131+Muhammad-Usama-07@users.noreply.github.com>
2026-05-21 00:17:42 +0500

    Add files via upload

--- school_lms_portal/settings.py
+++ school_lms_portal/settings.py
@@ -16,7 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # EduNest Apps
+    # school_lms_portal Apps
     'accounts',
     'lms',
     'portal',
